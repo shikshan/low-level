411b052b32b1a2a7b71c068cd515d4becae5fb45
Omkar Patil <ospatil@gmail.com>
2019-01-08 09:50:07 -0500

    Add hashbang

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 docker run --rm -it -v ~/work/ws/github/shikshan/low-level:/workspace/code ospatil/alpine-gcc bash
-# gcc -Wall uv.c -luv -Wl,-rpath,/usr/local/lib64 -o build/uv
 
 # To find out glibc version
 # ldd --version
